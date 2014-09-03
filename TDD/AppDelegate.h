2a3c955e5dd8879d7d29520898af0837589ad91c
SangChan Lee <sangchan82@gmail.com>
2014-09-03 11:49:56 +0900

    Initial Commit

---
+++ TDD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TDD
+//
+//  Created by SangChan on 2014. 9. 3..
+//  Copyright (c) 2014년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TDD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TDD
+//
+//  Created by SangChan on 2014. 9. 3..
+//  Copyright (c) 2014년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
